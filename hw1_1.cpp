4ece5c12a2b60d1a9d63573d184ca9ebc2164cf4
weiss21 <waisalirobleh@gmail.com>
2019-12-31 02:22:16 +0000

    add extra files

--- hw1_1.cpp
+++ hw1_1.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main() 
 {
-    cout << "Hello, World!";
+    ifstream myFile;
+    string fileName;
+    string line;
+   cout << "Enter input file name: " << endl;
+   cin >> fileName;
+   myFile.open(fileName.c_str());
+   cout << endl;
+   cout<< fileName + " is opened" << endl;
+   while(getline(myFile,line).good()) // To get you all the lines.
+        {
+	        getline(myFile,line); // Saves the line in STRING.
+	        cout<<line; // Prints our STRING.
+        }
+	myFile.close();
+	cout << "\n is closed";
+   
     return 0;
 }
